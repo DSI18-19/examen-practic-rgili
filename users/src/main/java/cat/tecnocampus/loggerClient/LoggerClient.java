564ec32561e4db29e7646535406d8b6f5457506b
rgili <rgili@edu.tecnocampus.cat>
2019-03-15 12:57:23 +0100

    last commit

--- users/src/main/java/cat/tecnocampus/loggerClient/LoggerClient.java
+++ users/src/main/java/cat/tecnocampus/loggerClient/LoggerClient.java
@@ -1,5 +1,8 @@
 package cat.tecnocampus.loggerClient;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
@@ -7,10 +10,17 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 @Service
 public class LoggerClient {
 
-    private static RestTemplate restTemplate;
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
-    private static final String URL_DELETE_USER ="http://localhost:8081/usersDelete/";
-    private static final String URL_CREATE_USER ="http://localhost:8081/usersCreate";
+    @Autowired
+    RestTemplate restTemplate;
+
+    private static final String URL_DELETE_USER ="http://logger/usersDelete/";
+    private static final String URL_CREATE_USER ="http://logger/usersCreate";
 
     public static final int CREATE_COMMUNICATION_SUCCESS = 1;
     public static final int DELETE_COMMUNICATION_SUCCESS = 2;
@@ -20,24 +30,22 @@ public class LoggerClient {
     @HystrixCommand(fallbackMethod = "loggerDeleteConnectionLost")
     public int comunicateDeleteUser(String username){
         String consulta = URL_DELETE_USER + username;
-        restTemplate = new RestTemplate();
         restTemplate.getForObject(consulta, String.class);
         return DELETE_COMMUNICATION_SUCCESS;
     }
 
 
-    public int loggerDeleteConnectionLost(){
+    public int loggerDeleteConnectionLost(String username){
         return DELETE_COMMUNICATION_FAIL;
     }
 
     @HystrixCommand(fallbackMethod = "loggerCreateConnectionLost")
     public int comunicateCreateUser(){
-        restTemplate = new RestTemplate();
         restTemplate.getForObject(URL_CREATE_USER, String.class);
         return CREATE_COMMUNICATION_SUCCESS;
     }
 
-    public int loggerCreateConnectionLost(){
+    public int loggerCreateConnectionLost(String username){
         return CREATE_COMMUNICATION_FAIL;
     }
 
